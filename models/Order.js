7cc4dc34541eaa3335fdc44e7a68f12b93cc0bb3
shalem <shalembadugu@gmail.com>
2026-05-11 20:45:37 +0530

    Created Sequelize database models

---
+++ models/Order.js
@@ -0,0 +1,36 @@
+import { DataTypes } from 'sequelize';
+import { sequelize } from './index.js';
+
+export const Order = sequelize.define('Order', {
+  userId: {
+  type: DataTypes.INTEGER,
+  allowNull: false
+},
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  orderTimeMs: {
+    type: DataTypes.BIGINT,
+    allowNull: false
+  },
+  totalCostCents: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  products: {
+    type: DataTypes.JSON,
+    allowNull: false
+  },
+  createdAt: {
+    type: DataTypes.DATE(3)
+  },
+  updatedAt: {
+    type: DataTypes.DATE(3)
+  },
+}, {
+  defaultScope: {
+    order: [['createdAt', 'ASC']]
+  }
+});
