bafb57ee21f9dbc0dec687d768d45f5c2d5dbcc2
shalem <shalembadugu@gmail.com>
2026-05-20 08:05:04 +0530

    Remove default cart seed data

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ if (productCount === 0) {
 
   await Product.bulkCreate(productsWithTimestamps);
   await DeliveryOption.bulkCreate(deliveryOptionsWithTimestamps);
-  await CartItem.bulkCreate(cartItemsWithTimestamps);
+  //await CartItem.bulkCreate(cartItemsWithTimestamps);
   //await Order.bulkCreate(ordersWithTimestamps);
 
   console.log('Default data added to the database.');
